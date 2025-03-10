813edb5c4a34ddb0c43239336e8b93b411c2790d
tnabi6 <tnabi6@gatech.edu>
2025-03-10 15:39:41 -0400

    Testing adding source files for watch standalone application

---
+++ wristwash_2.0 Watch App/wristwash_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  wristwash_2_0App.swift
+//  wristwash_2.0 Watch App
+//
+//  Created by Umme Nur on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct wristwash_2_0_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
